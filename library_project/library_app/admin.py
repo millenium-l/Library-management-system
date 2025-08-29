4e55e466819c2d045f1f43ee332a2303507d0dca
millenium-1 <louiskaroki2010@gmail.com>
2025-08-29 13:22:53 +0300

    nextcommit

--- library_project/library_app/admin.py
+++ library_project/library_app/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
-
+from .models import Profile, Book, IssuedBook
 # Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role')
+    search_fields = ('user__username', 'role')
+    list_filter = ('role',)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'isbn', 'category', 'total_copies', 'available_copies')
+    search_fields = ('title', 'author', 'isbn')
+    list_filter = ('category',)
+
+@admin.register(IssuedBook)
+class IssuedBookAdmin(admin.ModelAdmin):
+    list_display = ('book', 'user', 'issue_date', 'due_date', 'return_date', 'is_overdue_display')
+    search_fields = ('book__title', 'user__username')
+    list_filter = ('issue_date', 'due_date', 'return_date')
+
+    def is_overdue_display(self, obj):
+        return obj.is_overdue()
+    is_overdue_display.boolean = True
+    is_overdue_display.short_description = 'Overdue?'
