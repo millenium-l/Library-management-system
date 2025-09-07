0e0e8218139e06b4ec4bc84d45b3fa1041ef872b
millenium-1 <louiskaroki2010@gmail.com>
2025-09-07 19:58:52 +0300

    access

--- library_project/library_project/urls.py
+++ library_project/library_project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('login', permanent=False)),  # Redirect root to login
     path('admin/', admin.site.urls),
     path('library/', include('library_app.urls')),
     path('accounts/', include('accounts.urls')),
