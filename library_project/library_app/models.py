7d4ec5d3c0e4365ec9612bdbe93946de53272c8c
millenium-1 <louiskaroki2010@gmail.com>
2025-09-14 11:16:28 +0300

    profile_creation

--- library_project/library_app/models.py
+++ library_project/library_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Profile(models.Model):
@@ -14,6 +15,13 @@ class Profile(models.Model):
     def __str__(self):
         return f"{self.user.username if self.user else 'Unknown User'} - {self.role}"
 
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    else:
+        instance.profile.save()
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
